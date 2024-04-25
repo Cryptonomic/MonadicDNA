1e8613577169f8559a40a491b39cc958f95c1434
Aisha Opaluwa <47456520+Aeesh@users.noreply.github.com>
2024-04-25 13:54:31 +0100

    remove attestation delay (#61)

--- snipper-bot/src/utils/attestations.ts
+++ snipper-bot/src/utils/attestations.ts
@@ -1,13 +1,10 @@
-import { ethers } from "ethers";
 import { IComputedResult, IMonadicDNAVerifiedTrait } from "@/types";
-import { createAttestationError, retreiveResultsError } from "@/types/customError";
-import { getValue } from ".";
+import { createAttestationError } from "@/types/customError";
 
 const {
     SignProtocolClient,
     SpMode,
     EvmChains,
-    IndexService
 } = require('@ethsign/sp-sdk');
 
 const { privateKeyToAccount } = require('viem/accounts');
@@ -46,71 +43,3 @@ export async function createAttestation(passportId: string, computedResult: ICom
         throw createAttestationError;
     }
 }
-
-
-export async function getAllAttestationIds(indexingValue: string) {
-    const indexService = new IndexService('testnet');
-
-    try {
-        const res = await indexService.queryAttestationList( {
-            indexingValue
-        });
-
-        const attestationIds: string[] = res.rows
-            .filter((row: { schemaId: string }) => row.schemaId === config.verifiedTraitSchemaId)
-            .map((row: { id: string }) => row.id);
-
-        console.log(`Attestation ID for ${indexingValue}: ${attestationIds}`);
-
-        return attestationIds;
-    } catch (error) {
-        console.error('Error viewing results:', error);
-        throw retreiveResultsError;
-    }
-}
-
-export async function getResultsById(attestationId: string) {
-    const indexService = new IndexService('testnet');
-
-    try {
-        const res = await indexService.queryAttestation(attestationId);
-        const encodedData = res.data
-
-        let decodedData = ethers.AbiCoder.defaultAbiCoder().decode(
-            ['string', 'string', 'string', 'string'],
-            encodedData
-        );
-
-        const results: IMonadicDNAVerifiedTrait = {
-          "Passport ID": decodedData[0],
-          Provider: decodedData[1],
-          Trait: decodedData[2],
-          Value: getValue(decodedData[3])
-        };
-
-        console.log(`Attestation Result for ${attestationId}: ${results}`);
-        return {
-            id: res.id,
-            data: results
-        };
-    } catch (error) {
-        console.error('Error viewing results:', error);
-        throw retreiveResultsError;
-    }
-}
-
-// export async function queryAttestationById(attestationId: string) {
-//     const indexService = new IndexService('testnet');
-
-//     console.log()
-
-//     try {
-//         const res = await indexService.queryAttestation(attestationId);
-
-//         console.log(`Attestation Result for ${attestationId}: ${res}`);
-//         return res;
-//     } catch (error) {
-//         console.error('Error uploading file:', error);
-//         throw retreiveResultsError;
-//     }
-// }
