55b2f62ca38275fa64b58e46de1fc0c8fc2fca5a
Vishakh <vishakh@users.noreply.github.com>
2024-08-01 20:26:48 -0400

    Continuing an E2E test script for Nillion.

--- services/nillion-interactor/compile.sh
+++ services/nillion-interactor/compile.sh
@@ -5,4 +5,7 @@ echo "Compiling thrombosis.py"
 pynadac --target-dir binaries --generate-mir-json programs/thrombosis.py
 
 echo "Compiling muscle-perform.py"
-pynadac --target-dir binaries --generate-mir-json programs/muscle-perform.py 
+pynadac --target-dir binaries --generate-mir-json programs/muscle-perform.py
+
+echo "Compiling double.py"
+pynadac --target-dir binaries --generate-mir-json /tmp/double.py
